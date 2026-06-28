198e7d83a15d4a34a166cba81c4a719e9fe6c445
Modulo Developer <developer@modulo.run>
2026-06-28 15:06:40 +0100

    feat: add sidebar layout with navigation for all views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="min-h-screen">
-    <router-view />
-  </div>
+  <LoginView v-if="!isAuthenticated" />
+  <AppLayout v-else />
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { getAccessToken } from './lib/api/client'
+import LoginView from './views/LoginView.vue'
+import AppLayout from './components/AppLayout.vue'
+
+const isAuthenticated = computed(() => !!getAccessToken())
+</script>
