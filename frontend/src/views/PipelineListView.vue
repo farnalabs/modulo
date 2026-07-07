f3783d5adb050f2a8220642103038b5403314c3b
Modulo Developer <developer@modulo.run>
2026-07-07 15:02:30 +0100

    feat: merge /templates into /library, remove redundant templates page

--- frontend/src/views/PipelineListView.vue
+++ frontend/src/views/PipelineListView.vue
@@ -16,7 +16,7 @@
         </div>
           <router-link
             v-if="allPipelines.length > 0 && !loading"
-            to="/templates"
+            to="/library"
             class="px-4 py-2 bg-primary text-primary-foreground text-sm font-medium rounded-lg hover:brightness-110 transition-all"
             data-testid="pipeline-list-new-pipeline"
           >
