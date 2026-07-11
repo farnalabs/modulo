3ecae7c7d7b0ea0706ce488f3df3ea4eeca04e33
Modulo Developer <developer@modulo.run>
2026-07-11 22:47:07 +0100

    fix: add missing i18n key and SvgIcon icons (Mail, File)

--- frontend/src/components/SvgIcon.vue
+++ frontend/src/components/SvgIcon.vue
@@ -739,6 +739,38 @@
   </svg>
   <svg
     aria-hidden="true"
+    v-else-if="name === 'Mail'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <rect width="20" height="16" x="2" y="4" rx="2" />
+    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+  </svg>
+  <svg
+    aria-hidden="true"
+    v-else-if="name === 'File'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+    <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+  </svg>
+  <svg
+    aria-hidden="true"
     v-else
     xmlns="http://www.w3.org/2000/svg"
     width="16"
@@ -771,7 +803,7 @@ const knownIcons = [
   "CreditCard", "Copy", "Columns", "KeyRound", "Tag", "Puzzle",
   "Clock", "Cpu", "Zap", "Building", "ShieldQuestion", "Bell",
   "Container", "LayoutTemplate", "Map", "SlidersHorizontal", "Building2",
-  "Bot", "AlertTriangle", "GitCommit", "FlaskConical", "Search",
+  "Bot", "AlertTriangle", "GitCommit", "FlaskConical", "Search", "Mail", "File",
 ];
 
 watch(
